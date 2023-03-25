664feddc9812eff455d56c59d25adf6cfdcaf338
moton03 <kirari.moton@outlook.com>
2023-03-25 21:54:03 +0900

    Update

--- src/ImageAnalysis/ImageAnalysis/Form1.Designer.cs
+++ src/ImageAnalysis/ImageAnalysis/Form1.Designer.cs
@@ -28,12 +28,245 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            mainTableLayoutPanel = new TableLayoutPanel();
+            menuStrip = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            revertToolStripMenuItem = new ToolStripMenuItem();
+            toolToolStripMenuItem = new ToolStripMenuItem();
+            cropToolStripMenuItem = new ToolStripMenuItem();
+            rotateToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip = new StatusStrip();
+            toolStripStatusLabel = new ToolStripStatusLabel();
+            toolStripProgressBar = new ToolStripProgressBar();
+            mainPictureBoxPanel = new Panel();
+            mainPictureBox = new PictureBox();
+            openFileDialog = new OpenFileDialog();
+            saveFileDialog = new SaveFileDialog();
+            mainTableLayoutPanel.SuspendLayout();
+            menuStrip.SuspendLayout();
+            statusStrip.SuspendLayout();
+            mainPictureBoxPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)mainPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // mainTableLayoutPanel
+            // 
+            mainTableLayoutPanel.ColumnCount = 1;
+            mainTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            mainTableLayoutPanel.Controls.Add(menuStrip, 0, 0);
+            mainTableLayoutPanel.Controls.Add(statusStrip, 0, 2);
+            mainTableLayoutPanel.Controls.Add(mainPictureBoxPanel, 0, 1);
+            mainTableLayoutPanel.Dock = DockStyle.Fill;
+            mainTableLayoutPanel.Location = new Point(0, 0);
+            mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            mainTableLayoutPanel.RowCount = 3;
+            mainTableLayoutPanel.RowStyles.Add(new RowStyle());
+            mainTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            mainTableLayoutPanel.RowStyles.Add(new RowStyle());
+            mainTableLayoutPanel.Size = new Size(800, 450);
+            mainTableLayoutPanel.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            menuStrip.ImageScalingSize = new Size(24, 24);
+            menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, toolToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(800, 33);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, toolStripSeparator2, saveToolStripMenuItem, saveAsToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(54, 29);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(270, 34);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(267, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(270, 34);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(270, 34);
+            saveAsToolStripMenuItem.Text = "SaveAs";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(267, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(270, 34);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { revertToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(58, 29);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // revertToolStripMenuItem
+            // 
+            revertToolStripMenuItem.Name = "revertToolStripMenuItem";
+            revertToolStripMenuItem.Size = new Size(270, 34);
+            revertToolStripMenuItem.Text = "Revert";
+            // 
+            // toolToolStripMenuItem
+            // 
+            toolToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cropToolStripMenuItem, rotateToolStripMenuItem });
+            toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            toolToolStripMenuItem.Size = new Size(61, 29);
+            toolToolStripMenuItem.Text = "Tool";
+            // 
+            // cropToolStripMenuItem
+            // 
+            cropToolStripMenuItem.Name = "cropToolStripMenuItem";
+            cropToolStripMenuItem.Size = new Size(270, 34);
+            cropToolStripMenuItem.Text = "Crop";
+            cropToolStripMenuItem.Click += cropToolStripMenuItem_Click;
+            // 
+            // rotateToolStripMenuItem
+            // 
+            rotateToolStripMenuItem.Name = "rotateToolStripMenuItem";
+            rotateToolStripMenuItem.Size = new Size(270, 34);
+            rotateToolStripMenuItem.Text = "Rotate";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(65, 29);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(164, 34);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // statusStrip
+            // 
+            statusStrip.ImageScalingSize = new Size(24, 24);
+            statusStrip.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel, toolStripProgressBar });
+            statusStrip.Location = new Point(0, 418);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(800, 32);
+            statusStrip.TabIndex = 1;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            toolStripStatusLabel.Name = "toolStripStatusLabel";
+            toolStripStatusLabel.Size = new Size(78, 25);
+            toolStripStatusLabel.Text = "Standby";
+            // 
+            // toolStripProgressBar
+            // 
+            toolStripProgressBar.Name = "toolStripProgressBar";
+            toolStripProgressBar.Size = new Size(100, 24);
+            // 
+            // mainPictureBoxPanel
+            // 
+            mainPictureBoxPanel.AutoScroll = true;
+            mainPictureBoxPanel.Controls.Add(mainPictureBox);
+            mainPictureBoxPanel.Dock = DockStyle.Fill;
+            mainPictureBoxPanel.Location = new Point(3, 36);
+            mainPictureBoxPanel.Name = "mainPictureBoxPanel";
+            mainPictureBoxPanel.Size = new Size(794, 379);
+            mainPictureBoxPanel.TabIndex = 2;
+            // 
+            // mainPictureBox
+            // 
+            mainPictureBox.Location = new Point(0, 0);
+            mainPictureBox.Name = "mainPictureBox";
+            mainPictureBox.Size = new Size(794, 379);
+            mainPictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            mainPictureBox.TabIndex = 0;
+            mainPictureBox.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(mainTableLayoutPanel);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip;
+            Name = "Form1";
+            Text = "ImageAnalysis";
+            mainTableLayoutPanel.ResumeLayout(false);
+            mainTableLayoutPanel.PerformLayout();
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            mainPictureBoxPanel.ResumeLayout(false);
+            mainPictureBoxPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)mainPictureBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel mainTableLayoutPanel;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem revertToolStripMenuItem;
+        private ToolStripMenuItem toolToolStripMenuItem;
+        private ToolStripMenuItem cropToolStripMenuItem;
+        private ToolStripMenuItem rotateToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private StatusStrip statusStrip;
+        private ToolStripStatusLabel toolStripStatusLabel;
+        private ToolStripProgressBar toolStripProgressBar;
+        private OpenFileDialog openFileDialog;
+        private Panel mainPictureBoxPanel;
+        private PictureBox mainPictureBox;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private SaveFileDialog saveFileDialog;
     }
 }
--- src/ImageAnalysis/ImageAnalysis/Form1.cs
+++ src/ImageAnalysis/ImageAnalysis/Form1.cs
@@ -1,10 +1,86 @@
+using System.Windows.Forms;
+
 namespace ImageAnalysis
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp = null;
+        string path = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                toolStripStatusLabel.Text = "Wait";
+                toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 4);
+                path = openFileDialog.FileName;
+                toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 4);
+                bmp = new Bitmap(path);
+                toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 4);
+                MainPictureBoxUpdate();
+                toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 4);
+                toolStripProgressBar.Value = 0;
+                toolStripStatusLabel.Text = "Standby";
+            }
+        }
+
+        void MainPictureBoxUpdate()
+        {
+            mainPictureBox.Image = bmp;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (bmp == null)
+            {
+
+            }
+            else
+            {
+                toolStripStatusLabel.Text = "Wait";
+                toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 2);
+                bmp.Save(path);
+                toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 2);
+                toolStripProgressBar.Value = 0;
+                toolStripStatusLabel.Text = "Standby";
+            }
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (bmp == null)
+            {
+
+            }
+            else
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    toolStripStatusLabel.Text = "Wait";
+                    toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 2);
+                    bmp.Save(saveFileDialog.FileName);
+                    toolStripProgressBar.Value = toolStripProgressBar.Value + (100 / 2);
+                    toolStripProgressBar.Value = 0;
+                    toolStripStatusLabel.Text = "Standby";
+                }
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void cropToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
